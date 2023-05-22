87ca3142081f9b225b1fbba213850c967c6b9f9a
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-05-22 20:13:55 +0600

    this is the secend commit

---
+++ src/component/Saju/Saju.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Saju = () => {
+    return (
+        <div>
+            <h1>This is the saju side of the project ..</h1>
+        </div>
+    );
+};
+
+export default Saju;
