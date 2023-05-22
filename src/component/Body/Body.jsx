cdb86ade024ee730489e168670c7f37c8be552f2
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-05-22 20:06:42 +0600

    first commit

---
+++ src/component/Body/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div>
+            <h1>This is the body side in the project ..</h1>
+        </div>
+    );
+};
+
+export default Body;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiDX24d0JYI5PU004Xq_1dxFiXk9yVXJI",
+  authDomain: "module58-8eb59.firebaseapp.com",
+  projectId: "module58-8eb59",
+  storageBucket: "module58-8eb59.appspot.com",
+  messagingSenderId: "614374778068",
+  appId: "1:614374778068:web:c747d4ed4136fcde4a8741"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
